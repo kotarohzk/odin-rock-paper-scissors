aba0b09355dfdd57b453fdccd85a97742cefb5f2
scott <scottchowww@gmail.com>
2023-08-29 21:18:10 +0800

    feat: add helper function to capitalise string

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function getComputerChoice() {
   let choice = ["Rock", "Paper", "Scissors"];
   return choice[Math.floor(Math.random() * choice.length)];
 }
+
+function capitaliseWord(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
