3b26ee406810c182e5e17208d967f75b3860c67d
scott <scottchowww@gmail.com>
2023-08-29 21:18:52 +0800

    feat: add function to play a single round of RPS and announce result

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ function getComputerChoice() {
 function capitaliseWord(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+function playRound(playerSelection, computerSelection) {
+  let player = capitaliseWord(playerSelection);
+  let result;
+  let playerWin;
+
+  if (player == computerSelection) {
+    result = `It's A Tie. Both players played ${player}`;
+    playerWin = 0;
+  } else if (
+    (player == "Rock" && computerSelection == "Scissors") ||
+    (player == "Paper" && computerSelection == "Rock") ||
+    (player == "Scissors" && computerSelection == "Paper")
+  ) {
+    result = `You Win! ${player} beats ${computerSelection}`;
+    playerWin = 1;
+  } else {
+    result = `You Lose! ${computerSelection} beats ${player}`;
+    playerWin = -1;
+  }
+  console.log(result);
+  return playerWin;
+}
